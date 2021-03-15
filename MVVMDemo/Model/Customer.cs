cadd5ce33b493161a0ed9a0ae0b567dc314579de
Bip <adrien.grandpierre@edu.itescia.fr>
2021-03-15 16:31:50 +0100

    Add project files.

---
+++ MVVMDemo/Model/Customer.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MVVMDemo.Model
+{
+    class Customer : INotifyPropertyChanged
+    {
+        private string firstName;
+
+        public string FirstName
+        {
+            get { return firstName; }
+            set
+            {
+                if (firstName != value)
+                {
+                    firstName = value;
+                    MyPropertyChanged("FirstName");
+                    MyPropertyChanged("FullName");
+                }
+            }
+        }
+
+        private string lastName;
+
+        public string LastName
+        {
+            get { return lastName; }
+            set
+            {
+                if (lastName != value)
+                {
+                    lastName = value;
+                    MyPropertyChanged("LastName");
+                    MyPropertyChanged("FullName");
+                }
+            }
+        }
+
+        public string FullName
+        {
+            get
+            {
+                return firstName + " " + lastName;
+            }
+        }
+
+
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void MyPropertyChanged(string property)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(property));
+            }
+        }
+    }
+}
---
+++ MVVMDemo/ViewModel/CustomerViewModel.cs
@@ -0,0 +1,34 @@
+﻿using MVVMDemo.Model;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MVVMDemo.ViewModel
+{
+    class CustomerViewModel
+    {
+        public ObservableCollection<Customer> Customers
+        {
+            get;
+            set;
+        }
+
+        public void LoadCustomers()
+        {
+            ObservableCollection<Customer> customers = new ObservableCollection<Customer>();
+
+            customers.Add(new Customer { FirstName = "Anthony", LastName = "Custodio" });
+            customers.Add(new Customer { FirstName = "Arnaud", LastName = "Chretien" });
+            customers.Add(new Customer { FirstName = "Adrien", LastName = "Grandpierre" });
+            customers.Add(new Customer { FirstName = "Lucas", LastName = "Burette" });
+            customers.Add(new Customer { FirstName = "Viktor", LastName = "Stoyanov" });
+
+            Customers = customers;
+        }
+
+
+    }
+}
